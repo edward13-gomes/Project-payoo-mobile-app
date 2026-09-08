84205eb8eed00a1d7fa7e6686e4d43917b2bc1cd
Edward Sharen Gomes <sharengomes2004@gmail.com>
2026-09-08 22:33:38 +0600

    add monery few part done

---
+++ script.js
@@ -0,0 +1,21 @@
+// login button funtionality
+
+document.getElementById('loginButton')
+.addEventListener('click',function(e){
+    e.preventDefault();
+    const moblieNumber =12345678910;
+    const pinNumber=1234;
+
+    const mobileNumberValue=document.getElementById('mobile-number').value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue=document.getElementById('pin-number').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberValueConverted===moblieNumber && pinNumberValueConverted===pinNumber){
+        window.location.href="home.html"
+    }else{
+           alert('Invalid credentials');
+    }
+
+})
